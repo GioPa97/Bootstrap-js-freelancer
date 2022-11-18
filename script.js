d5bd5e3468e862156801b0f7ad903344e01b92b4
GioPa97 <giovannipagliara97@gmail.com>
2022-11-18 17:50:16 +0100

    Aggiunta stampa in console

--- script.js
+++ script.js
@@ -77,6 +77,19 @@ function submitForm(event) {
     let commissione = tariffaServizio(servizioOfferto);
     let prezzofinale = calcoloPrezzo(ore, commissione, controlloSconto).toFixed(2);
     document.getElementById("prestazione").innerHTML = "Il prezzo finale è di: " + prezzofinale + "&euro;";
+    
+    console.log(
+        `
+    INFO===========================================================================
+    "Il monte ore selezionato è di:                      " ${ore}          "ore"
+    "La tariffa oraria per la commissione selezionata è: " ${commissione}  "€"
+    "Il prezzo finale è di:                              " ${prezzofinale} "€"
+    ===============================================================================
+    `
+    );
+
+   
+
 }
 
 
@@ -99,6 +112,7 @@ function tariffaServizio(tipoDiLavoro) {
             break;
     }
     return (tariffaServizio);
+    
 }
 
 //funzione per calcolare lo sconto se vi è il codice corretto;
